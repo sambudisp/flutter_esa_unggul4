d8b8e00dbf2a332b6a90fe20e38b815d1bc118d9
Sambudi Sugya Putra <sambudisp@gmail.com>
2019-11-16 15:08:50 +0700

    get dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:movie_db/ui/home_screen.dart';
 
 void main() => runApp(MaterialApp(
-  home: HomeScreen(),
-));
-
+      home: HomeScreen(),
+    ));
